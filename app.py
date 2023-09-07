6f4a4a2a63e6b8a4939b1f6027f94f1e1188a646
sarahcyphz <sarah.cyphers@neudesic.com>
2023-09-07 15:45:43 -0400

    Converts prompts to functions

--- app.py
+++ app.py
@@ -17,28 +17,45 @@ def menu():
 
     while (user_input := input(MENU_PROMPT)) != "5":
         if user_input == "1":
-            name = input("Enter bean name: ")
-            method = input("Enter how you've prepared it: ")
-            rating = int(input("Enter your rating score (0-100): "))
-
-            database.add_bean(connection, name, method, rating)
+            prompt_add_new_bean(connection)
         elif user_input == "2":
-            beans = database.get_all_beans(connection)
-
-            for bean in beans:
-                print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+            prompt_see_all_beans(connection)
         elif user_input == "3":
-            name = input("Enter bean name to find: ")
-            beans = database.get_beans_by_name(connection, name)
-
-            for bean in beans:
-                print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+            prompt_find_bean(connection)
         elif user_input == "4":
-            name = input("Enter bean name to find: ")
-            best_method = database.get_best_preparation_for_bean(connection, name)
-
-            print(f"The best preparation method for {name} is: {best_method[2]} ")
+            prompt_find_best_method(connection)
         else:
             print("invalid input")
-# testing
+
+
+def prompt_add_new_bean(connection):
+    name = input("Enter bean name: ")
+    method = input("Enter how you've prepared it: ")
+    rating = int(input("Enter your rating score (0-100): "))
+
+    database.add_bean(connection, name, method, rating)
+
+
+def prompt_see_all_beans(connection):
+    beans = database.get_all_beans(connection)
+
+    for bean in beans:
+        print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+
+
+def prompt_find_bean(connection):
+    name = input("Enter bean name to find: ")
+    beans = database.get_beans_by_name(connection, name)
+
+    for bean in beans:
+        print(f"{bean[1]} ({bean[2]}) - {bean[3]}/100")
+
+
+def prompt_find_best_method(connection):
+    name = input("Enter bean name to find: ")
+    best_method = database.get_best_preparation_for_bean(connection, name)
+
+    print(f"The best preparation method for {name} is: {best_method[2]} ")
+
+
 menu()
